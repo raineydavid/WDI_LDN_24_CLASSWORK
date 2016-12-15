b0107273a38cb5af0636ef470f4c01b476e0f49e
rainede <rainey@techgiver.com>
2016-12-15 16:54:21 +0000

    basic express with logging

--- rainede/w03d04/intro-to-express2/index.js
+++ rainede/w03d04/intro-to-express2/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+//Logging
+app.use(function(req, res, next){
+  console.log('In comes a ' + req.method + 'to ' + req.url);
+  next();
+});
+
 app.use(function(req, res){
   res.writeHead(200, {'content-Type': 'text/plain'});
   res.end('Hello world');
