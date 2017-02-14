8cc88eae5d212a842282d689d8d09be31797309c
rainede <rainey@techgiver.com>
2017-02-14 17:06:07 +0000

    add more session info

--- rainede/w11d02/authentication-rails/authentication-example/app/controllers/application_controller.rb
+++ rainede/w11d02/authentication-rails/authentication-example/app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :current_user
+  helper_method :current_user
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
-   raise
+  end
+
+  def logged_in?
+    !!@current_user
   end
 
 end
--- rainede/w11d02/authentication-rails/authentication-example/config/routes.rb
+++ rainede/w11d02/authentication-rails/authentication-example/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  root 'users#index'
   resources :users, only: [:index, :create]
   get 'register', to: 'users#new'
   get 'login', to: "sessions#new"
   delete 'logout', to: "sessions#destroy"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :sessions, only: [:create]
 end
