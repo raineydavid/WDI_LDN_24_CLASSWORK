9589ebef6d96e40508f8fccb5589625052b6134a
rainede <rainey@techgiver.com>
2017-02-15 14:23:08 +0000

    update the model for user and routes file

--- rainede/w11d03/twitter/config/routes.rb
+++ rainede/w11d03/twitter/config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   scope :api do
     resources :posts
     resources :users
+    post 'register', to: "authentications#register"
+    post 'login', to: "authentications#login"
   end
 end
