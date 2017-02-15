ad373f2b57ddca934d57d4a685dae24accfe5de9
rainede <rainey@techgiver.com>
2017-02-15 11:03:15 +0000

    Manipulating how the json file looks by updating the user serializer

--- rainede/w11d03/twitter/app/serializers/user_serializer.rb
+++ rainede/w11d03/twitter/app/serializers/user_serializer.rb
@@ -1,3 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :first_name, :last_name
+  attributes :id, :username, :first_name, :last_name, :full_name
+
+  def full_name
+    '#{object.first_name} #{object.last_name}'
+  end
 end
