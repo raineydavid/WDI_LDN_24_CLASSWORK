881afacf896cabe2d7d54b5c90644ca3523e769c
rainede <rainey@techgiver.com>
2017-02-15 11:11:36 +0000

    moving from user serializer to model

--- rainede/w11d03/twitter/app/models/user.rb
+++ rainede/w11d03/twitter/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_many :posts
   validates :username, presence: true, uniqueness: true
+
+  def full_name
+    "#{object.first_name} #{object.last_name}"
+  end
 end
