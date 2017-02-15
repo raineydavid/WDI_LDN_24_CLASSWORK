8ecebd4d18b20898f7d95d428621216a9a5a42a5
rainede <rainey@techgiver.com>
2017-02-15 11:23:16 +0000

    added scope :api so that routes are only reachable from /api

--- rainede/w11d03/twitter/config/routes.rb
+++ rainede/w11d03/twitter/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :posts
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope :api do
+    resources :posts
+    resources :users
+  end
 end
