afac14d5e8da3bdfab3f0f521e8fa8e0d99a3e9f
rainede <rainey@techgiver.com>
2017-02-15 11:05:02 +0000

    Serializer only works with double quotes

--- rainede/w11d03/twitter/app/serializers/user_serializer.rb
+++ rainede/w11d03/twitter/app/serializers/user_serializer.rb
@@ -2,6 +2,6 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :first_name, :last_name, :full_name
 
   def full_name
-    '#{object.first_name} #{object.last_name}'
+    "#{object.first_name} #{object.last_name}"
   end
 end
