5311256de690c0c4a031e5673d493cf1e5032933
rainede <rainey@techgiver.com>
2017-02-15 10:59:27 +0000

    Add data to seeds file and run rails db:seed

--- rainede/w11d03/twitter/db/seeds.rb
+++ rainede/w11d03/twitter/db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Post.destroy_all
+
+u1 = User.create! username: "wants_boyfriend", first_name: "celine", last_name: "brown_hair"
+u2 = User.create! username: "pretends_she_doesnt_want_boyfriend", first_name: "gwen", last_name: "trannoy"
+u3 = User.create! username: "doesnt_like_tongue_man", first_name: "dora", last_name: "the_explorer"
+
+p1 = u1.posts.create! body: "No time go out  and meet people #wontspeaktoanyoneanywaywhenido"
+p2 = u2.posts.create! body: "So busy with work, not interested; wen out 6 times last week #workingnotworking"
+p3 = u3.posts.create! body: "Erlang is amazing. #whatarewetalkingabout"
