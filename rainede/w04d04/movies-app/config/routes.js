bd4f90a4b8a919ca75db9576b45307675b8af999
rainede <rainey@techgiver.com>
2017-08-25 10:15:15 +0100

    updating routes file

--- rainede/w04d04/movies-app/config/routes.js
+++ rainede/w04d04/movies-app/config/routes.js
@@ -1,1 +1,17 @@
 const router = require('express').Router();
+const movies = require('../controllers/movies');
+
+router.route('/').get((req, res) => res.render('home'));
+
+router.route('/movies')
+  .get(movies.index)
+  .post(movies.create);
+
+router.route('/movies/new')
+  .get(movies.new);
+
+router.route('/movies/:id')
+  .get(movies.show);
+
+
+module.exports = router;
